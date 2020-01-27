894e3f00766c14235a5d1f83769e9a6b4b280f20
Betty42 <55029687+Betty42@users.noreply.github.com>
2020-01-27 18:30:03 +0200

    Update subnets.tf

--- terraform/subnets.tf
+++ terraform/subnets.tf
@@ -1,7 +1,7 @@
 resource "aws_subnet" "MidProject-Subnet-Priv" {
   count             = "2"
   availability_zone = "${element(var.azs,count.index)}"
-  cidr_block        = "${cidrsubnet(aws_vpc.OpsSchool-MidProject-VPC.cidr_block, 8, count.index)}" #(understand)
+  cidr_block        = "${cidrsubnet(aws_vpc.OpsSchool-MidProject-VPC.cidr_block, 8, count.index)}"
   vpc_id            = "${aws_vpc.OpsSchool-MidProject-VPC.id}"
 
   tags = {
@@ -12,7 +12,7 @@ resource "aws_subnet" "MidProject-Subnet-Priv" {
 resource "aws_subnet" "MidProject-Subnet-Pub" {
   count                   = "2"
   availability_zone       = "${element(var.azs,count.index)}"
-  cidr_block              = "${cidrsubnet(aws_vpc.OpsSchool-MidProject-VPC.cidr_block,8,count.index + 2)}" #(understand)
+  cidr_block              = "${cidrsubnet(aws_vpc.OpsSchool-MidProject-VPC.cidr_block,8,count.index + 2)}"
   map_public_ip_on_launch = true ##(understand)
   vpc_id            = "${aws_vpc.OpsSchool-MidProject-VPC.id}"
 
