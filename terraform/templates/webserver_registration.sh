5ffd9e45fbebb1f342d8a90e853c71413beb0416
Betty42 <55029687+Betty42@users.noreply.github.com>
2020-01-29 23:51:59 +0200

    Update webserver_registration.sh

--- terraform/templates/webserver_registration.sh
+++ terraform/templates/webserver_registration.sh
@@ -7,15 +7,37 @@ apt-get update
 apt --assume-yes install apache2
 systemctl start apache2.service
 
-tee /etc/consul.d/webserver.json > /dev/null <<EOF
+# Install Jenkins
+sudo apt-get update -y
+sudo apt-get install -y default-jre
+
+echo "* Import GPG key"
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+sudo add-apt-repository "deb https://pkg.jenkins.io/debian-stable binary/"
+sudo apt-get update
+sudo apt-get -y install jenkins
+sudo apt-get -y install jq
+
+echo "Tool for handling xml files"
+sudo apt-get install xmlstarlet
+
+echo "* Start Jenkins without asking to install plugins"
+export JAVA_OPTS=-Djenkins.install.runSetupWizard=false
+
+sudo service jenkins start
+
+sudo wget http://localhost:8080/jnlpJars/jenkins-cli.jar -O /var/lib/jenkins/jenkins-cli.jar
+ADMIN_PASSW=$(sudo bash -c "cat /var/lib/jenkins/secrets/initialAdminPassword")
+
+tee /etc/consul.d/jenkins.json > /dev/null <<EOF
 {
   "service":
-  {"name": "webserver",
-   "tags": ["apache"],
-   "port": 80,
-   "check": {
-     "name": "port 80 tcp check",
-     "TCP": "localhost:80",
+  {"name": "jenkins",
+   "tags": ["jenkins-ci"],
+   "port": 8080,
+   "checks": {
+     "name": "jenkins 8080 port check",
+     "http": "http://localhost:8080",
      "interval": "20s"
     }
   }
